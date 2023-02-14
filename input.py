ad388e9b835fc3d51fede1e6023b597de22fba54
mohamed shames <mohamedhames900@gmail.com>
2023-02-14 19:47:33 +0200

    first commit

---
+++ input.py
@@ -0,0 +1,7 @@
+name = input("what's your name ?")
+age = int(input("what's your age ?"))
+email = input("what's your email ?")
+password = input("what's your password ?")
+phone = input("what's your phone ?")
+
+print("your name is {0} and your age is {1} and your email is {2} and your password is {3} and your phone is {4}".format(name, age, email, password, phone))
